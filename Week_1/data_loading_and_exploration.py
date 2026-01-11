176b8d785945da62da92679dfb846b323d8a0c86
Eswar Pesala <164545817+EswarPesala@users.noreply.github.com>
2026-01-11 22:40:57 +0530

    Update data_loading_and_exploration.py

--- Week_1/data_loading_and_exploration.py
+++ Week_1/data_loading_and_exploration.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv(
-    r"C:\Users\pilla\OneDrive\Desktop\Multi-Model-AI-Agent\data\cardio_train.csv",
+    r"C:\To learn\Infosys_Internship_6_0\AI_HEALTH_DIAGNOSTICS\archive_cbc\CBC data_for_meandeley_csv.csv",
     sep=";"
 )
 
 print(df.head())
 print(df.info())
 print(df.describe())
+
 print(df.shape)
